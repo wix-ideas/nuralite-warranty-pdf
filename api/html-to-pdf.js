8867e73fb407a8ffe9c864a5641ee281da11738a
Wix Ideas <hello@wixideas.com>
2025-12-01 09:58:30 +0100

    Reduce top margin in PDF generation from 15mm to 10mm

--- api/html-to-pdf.js
+++ api/html-to-pdf.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
       format: 'A4',
       printBackground: true,
       margin: {
-        top: '15mm',
+        top: '10mm',
         bottom: '15mm',
         left: '10mm',
         right: '10mm'
